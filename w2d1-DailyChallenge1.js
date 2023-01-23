a2cf7f24f48300944a69963d86075d450d53c2da
yushi0212 <122863473+yushi0212@users.noreply.github.com>
2023-01-23 13:08:06 +0400

    Add files via upload

---
+++ w2d1-DailyChallenge1.js
@@ -0,0 +1,17 @@
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('kiwi');
+console.log(fruits);
+
+fruits.sort();
+console.log(fruits);
+
+fruits.splice(0,1);
+console.log(fruits);
+
+fruits.reverse();
+console.log(fruits);
